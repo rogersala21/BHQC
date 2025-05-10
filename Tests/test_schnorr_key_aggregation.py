0b3fd3b26d3fdc7d01475647f82a38ddb8dcb7d6
rogersala21 <rogersalamimo@gmail.com>
2025-05-10 21:23:34 +0200

    bitcoinkeygenFROMWIF.py test to produce a valid signature with the output that test_schnorr_key_aggregation.py gave (an aggregated privatekey) seems that all conversions work fine and the signature is valid :)

--- Tests/test_schnorr_key_aggregation.py
+++ Tests/test_schnorr_key_aggregation.py
@@ -1,5 +1,6 @@
 from coincurve import PrivateKey, PublicKey
 from bitcoinutils.keys import PublicKey as BitcoinPublicKey
+from bitcoinutils.keys import PrivateKey as BitcoinPrivateKey
 from bitcoinutils.setup import setup
 
 
@@ -16,9 +17,8 @@ priv3 = PrivateKey()
 
 print(priv1.secret.hex())
 print(priv2.secret.hex())
-
-
 print(priv3.secret.hex())
+
 
 # Transform the privatekeys to integers
 x1 = int.from_bytes(priv1.secret, 'big')
@@ -73,7 +73,7 @@ print("P1 + P2 + P3 =", agg_point_associative.format().hex())
 print("Agg from sec3 =", agg_secret_associative.public_key.format().hex())
 print("Match?       =", agg_point_associative.format() == agg_secret_associative.public_key.format())
 
-
+print("-------------------------Bitcoinutils Pubkey transformation---------------------------")
 # Transform public keys from coincurve to bitcoinutils
 pub1hex = pub1.format().hex()
 print("Pub1 hex:", pub1hex)
@@ -81,3 +81,23 @@ pub1btc = BitcoinPublicKey.from_hex(pub1hex)
 print("Pub1 btc:", pub1btc.to_hex())
 taprootpub = pub1btc.get_taproot_address()
 print("Taproot:", taprootpub.to_string())
+
+print("-------------------------Aggregated taproot---------------------------")
+# Trasform aggregated public key from coincurve to bitcoinutils
+agg_pointhex = agg_point2.format().hex()
+print("Agg_point2 hex:", agg_pointhex)
+agg_pointbtc = BitcoinPublicKey.from_hex(agg_pointhex)
+print("Agg_point2 btc:", agg_pointbtc.to_hex())
+taprootagg = agg_pointbtc.get_taproot_address()
+print("Agg_point2 Taproot:", taprootagg.to_string())
+
+print("-------------------------Bitcoinutils Privatekey transformation---------------------------")
+# Transform the coincurve private keys to bitcoinutils private keys (this will be used for the QC protocol)
+print(priv3.secret)
+prova1 = BitcoinPrivateKey.from_bytes(priv3.secret)
+print(prova1.to_bytes())
+
+print("------------------------")
+# Transform the aggregated private key to bitcoinutils private key to do a test tx
+privagg = BitcoinPrivateKey.from_bytes(agg_secret2.secret)
+print("\nPrivate key WIF:", privagg.to_wif(compressed=True))
