f527255b8846bae9693815774fffa7ef632f2c43
rogersala21 <rogersalamimo@gmail.com>
2025-06-11 20:47:52 +0200

    ask for private key txt done

--- src/s5_WalletReadyKeyRetriever.py
+++ src/s5_WalletReadyKeyRetriever.py
@@ -18,25 +18,42 @@ def check_data_correctness(introduced_taproot_address):
 
 def main():
     print("Starting to check data correctness...")
-    response2 = input("Make sure you have the public keys of all participants in the folder /outputs/coordinator/key_agg_input. And the ECIES outputs in /outputs/coordinator/honeypot_commitment. Do you want to continue? (yes/no): ")
-    if response2.lower() == "yes":
-        introduced_taproot_address = input("Please introduce the honeypot taproot address: ")
-        secp192r1_pub = check_data_correctness(introduced_taproot_address)
-    elif response2.lower() == "no":
-        print("Exiting...")
-        sys.exit(0)
-    else:
-        print("Invalid input. Please enter 'yes' or 'no'.")
-        sys.exit(1)
+    while True:
+        data_correctness_response = input(
+            "Make sure you have the public keys of all participants in the folder ../outputs/coordinator/key_agg_input. "
+            "And the ECIES outputs in ../outputs/coordinator/honeypot_commitment. Do you want to continue? (yes/no): "
+        )
+        if data_correctness_response.lower() == "yes":
+            introduced_taproot_address = input("Please introduce the honeypot taproot address: ")
+            secp192r1_pub = check_data_correctness(introduced_taproot_address)
+            break
+        elif data_correctness_response.lower() == "no":
+            print("Exiting...")
+            sys.exit(0)
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
 
-    #TODO: check if the PRIVATE key is in the expected format (compressed, hex, etc.)
-    private_key_secp192r1 = input(f"Please introduce the secp192r1 private key corresponding to public key {secp192r1_pub} in ???? format: ")
+    print("Strarting to decrypt ECIES outputs...")
+    while True:
+        secp192r1_private_response = input(f"Make sure you have the secp192r1 private key corresponding to public key {secp192r1_pub} in PEM format (PKCS8, unencrypted) into ../outputs/stealer/*.txt Do you want to continue? (yes/no): ")
+        if secp192r1_private_response.lower() == "yes":
+
+            break
+        elif secp192r1_private_response.lower() == "no":
+            print("Exiting...")
+            sys.exit(0)
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
 
     #TODO: DECRYPTION OF ALL ECIES OUTPUTS, SAVE THEM IN A LIST
 
     #TODO: AGGREGATION OF ALL DECRYPTED KEYS, AND GENERATE THE TWEAKED PRIVATE KEY (HONEYPOT PRIVATE KEY)
 
     #TODO: CONVERT THE HONEYPOT PRIVATE KEY TO WIF AND THEN USE DESCPIPTOR.PY TO GENERATE THE DESCRIPTOR READY TO IMPORT INTO BITCOIN CORE AND ALSO SAVE IT IN A FILE (DESCRIPTOR + HONEYPOT PRIVATE KEY IN WIF FORMAT)
+
+
+
 
 
 
