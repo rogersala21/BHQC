2067ec938e661aee295be6c687822b99b81a8bbc
rogersala21 <rogersalamimo@gmail.com>
2025-09-19 17:19:23 +0200

    pubkey btc and secp192r1 aggregations

--- src/c2_PublicKeyAggregator.py
+++ src/c2_PublicKeyAggregator.py
@@ -1,92 +1,60 @@
+import json
 import os
 from coincurve import PublicKey
-import hashlib
 from tinyec import registry
 from tinyec.ec import Point
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
 
-PUBKEY_DIR = "../outputs/coordinator/key_agg_input"
+PROOF_DIR = "../outputs/participant/proofs"
 OUTPUTS_DIR = "../outputs/coordinator/key_agg_output"
 
-def load_public_keys(pubkey_dir):
+def load_public_keys(proof_dir):
     # We iterate through all public key files in the specified directory and create a list of PublicKey objects.
-    pubkeylist = []
-    for filename in os.listdir(pubkey_dir):
-        file_path = os.path.join(pubkey_dir, filename)
-        if os.path.isfile(file_path) and filename.endswith(".txt"):
+    btc_pubkeylist = []
+    secp192_pubkeylist = []
+    for filename in os.listdir(proof_dir):
+        file_path = os.path.join(proof_dir, filename)
+        if os.path.isfile(file_path) and filename.startswith("proof_") and filename.endswith(".json"):
             with open(file_path, "r") as f:
-                content = f.read()
-                pubkey_bytes = bytes.fromhex(content)
-                pubkey_cc = PublicKey(pubkey_bytes)
-                pubkeylist.append(pubkey_cc)
-    return pubkeylist
+                data = json.load(f)
+                btc_pubkey_coords = data.get("public_key_btc") # CHANGE FOR WHATEVER WE USE IN THE PROOF JSON
+                secp192_pubkey_coords = data.get("enc_point_k") # CHANGE FOR WHATEVER WE USE IN THE PROOF JSON
+                if btc_pubkey_coords is None and secp192_pubkey_coords is None:
+                    print(f"Warning: No public_key found in {filename}")
+                    continue
+                # Create PublicKey object from coordinates
+                if btc_pubkey_coords is not None:
+                    x = btc_pubkey_coords[0]
+                    y = btc_pubkey_coords[1]
+                    pubkey_btc = PublicKey.from_point(x, y)
+                    btc_pubkeylist.append(pubkey_btc)
+                # Create PublicKey coordinates list
+                if secp192_pubkey_coords is not None:
+                    x = secp192_pubkey_coords[0]
+                    y = secp192_pubkey_coords[1]
+                    secp192_pubkeylist.append((x, y))
 
-def aggregate_public_keys(pubkeylist):
+    print("SECP192 Public Key List:")
+    for pk_bytes in secp192_pubkeylist:
+        print(pk_bytes)
+    return btc_pubkeylist, secp192_pubkeylist
+
+def aggregate_secp192r1_pubkeys(coords_list):
+    # Combine the secp192r1 public keys into a single aggregated public key
+    curve = registry.get_curve('secp192r1')
+    agg_point = None
+    for x, y in coords_list:
+        point = Point(curve, x, y)
+        if agg_point is None:
+            agg_point = point
+        else:
+            agg_point += point
+    return agg_point
+
+def aggregate_btc_pubkeys(pubkeylist):
     # Combine the public keys into a single aggregated public key
     return PublicKey.combine_keys(pubkeylist)
-
-def hash_public_key(agg_point):
-    # Convert the aggregated public key to bytes to hash it
-    agg_point_hex = agg_point.format().hex()
-    agg_point_bytes = bytes.fromhex(agg_point_hex)
-    # Hash the aggregated public key using SHA-256
-    hash_value = hashlib.sha256(agg_point_bytes).digest()
-    #print(hash_value.hex())
-    return hash_value
-
-def find_valid_curve_point(hash_value, curve, max_attempts=100):
-    # Get curve parameters (a, b and p) for secp192r1
-    a = curve.a
-    b = curve.b
-    p = curve.field.p # secp192r1 prime field size
-
-    valid_point_found = False # Boolean to track if a valid point is found
-    hash_attempts = 1 # Counter for hash attempts
-
-    while not valid_point_found and hash_attempts < max_attempts:
-        x_bytes = hash_value[:24]  # Truncate to the first 24 bytes for secp192r1
-        x_candidate = int.from_bytes(x_bytes, 'big') # Convert bytes to integer to get the x coordinate
-        #print("x_candidate before mod p:", x_candidate)
-
-        # Make sure x is within field range of secp192r1
-        x_candidate = x_candidate % p # Apply modulo p to ensure x is within the prime field size
-        #print("x_candidate after mod p:", x_candidate)
-
-        # Calculate right side of equation: y² = x³ + ax + b (mod p)
-        right_side = (pow(x_candidate, 3, p) + (a * x_candidate) % p + b) % p # Here we calculate the right side of the elliptic curve equation to find y²
-
-        # Check if y²(right_side) has a square root in the field, which means we can find a valid y coordinate for the given x
-        # This happens in elliptic curves because not all x values will yield a valid y value on the curve,
-        # as finite fields only have a limited set of numbers that have square roots and complex numbers do not exist in finite fields.
-        # This uses Euler's criterion to check if right_side is a quadratic residue modulo p, and therefore, a square root exists.
-        is_quadratic_residue = pow(right_side, (p - 1) // 2, p) == 1
-
-        if is_quadratic_residue:
-            # Calculate y doing the modular square root
-            y = pow(right_side, (p + 1) // 4, p)
-
-            try:
-                # Create a point directly using the Point class from tinyec
-                point = Point(curve, x_candidate, y)
-                #print(f"Valid point found after {hash_attempts} hashes!")
-                #print(f"Hash value: {hash_value.hex()}")
-                #print(f"Point: x={point.x}, y={point.y}")
-                #print(x_candidate)
-                valid_point_found = True
-                return point, hash_attempts
-            except Exception as e:
-                print(f"Error creating point: {e}")
-                hash_value = hashlib.sha256(hash_value).digest()
-                hash_attempts += 1
-        else:
-            # No valid y for this x, hash again
-            hash_value = hashlib.sha256(hash_value).digest()
-            hash_attempts += 1
-            #print(f"No valid point yet, attempts: {hash_attempts-1}")
-
-    print(f"Failed to find valid point after {hash_attempts} hash attempts")
-    return None, hash_attempts
 
 def serialize_point(point):
     # Format as uncompressed point (04 + x + y)
@@ -94,27 +62,25 @@ def serialize_point(point):
     y_bytes = point.y.to_bytes(24, byteorder='big')  # 24 bytes for y secp192r1
     encoded_point = b'\x04' + x_bytes + y_bytes
 
-    # Create public key from the encoded point using cryptography library to match the format used in ECIES script
+    # Create public key from the encoded point using cryptography library
     public_key = ec.EllipticCurvePublicKey.from_encoded_point(
         ec.SECP192R1(),
         encoded_point
     )
 
-    # Get compressed format (the one that ECIES script uses)
+    # Get compressed format
     compressed_bytes = public_key.public_bytes(
         encoding=serialization.Encoding.X962, # X9.62 standard for elliptic curve public keys
         format=serialization.PublicFormat.CompressedPoint # Compressed point format (02 or 03 prefix)
     )
 
-
-    #print("Compressed public key:", compressed_bytes.hex())
     return compressed_bytes
 
-def is_point_at_infinity(pubkey):
+def is_point_at_infinity_btc(pubkey):
     # Checks if the public key bytes are all zeros (invalid key)
     return pubkey.format() == b'\x00' * len(pubkey.format())
 
-def is_generator_point(pubkey):
+def is_generator_point_btc(pubkey):
     # secp256k1 generator point (compressed)
     generator_bytes = bytes.fromhex(
         "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798"
@@ -122,52 +88,43 @@ def is_generator_point(pubkey):
     generator = PublicKey(generator_bytes)
     return pubkey.format() == generator.format()
 
-def main():
-    pubkeylist = load_public_keys(PUBKEY_DIR)
-    #print("List of public keys:", [p.format().hex() for p in pubkeylist])
+def is_point_at_infinity_secp192r1(point):
+    # Checks if the point is at infinity
+    return point is None or (hasattr(point, "inf") and point.inf)
 
-    agg_point = aggregate_public_keys(pubkeylist)
-    #print("Aggregated public key:", agg_point.format().hex())
-
-    #from coincurve import PrivateKey
-    #priv_key = PrivateKey(b'\x00' * 31 + b'\x01')
-    #pub_key = priv_key.public_key
-    #agg_point = pub_key
-
-    #from coincurve import PrivateKey
-    #priv_key = PrivateKey(b'\x00' * 31 + b'\x00')
-    #pub_key = priv_key.public_key
-    #agg_point = pub_key
-
-    if is_point_at_infinity(agg_point):
-        print("Error!!! Aggregated public key is point at infinity (private key = 0).")
-        return
-    elif is_generator_point(agg_point):
-        print("Error!!! Aggregated public key is generator (private key = 1).")
-        return
-    else:
-        print("Aggregated public key is valid.")
-
-
-    hash_value = hash_public_key(agg_point)
-
+def is_generator_point_secp192r1(point):
+    # Checks if the point is the generator of secp192r1
     curve = registry.get_curve('secp192r1')
-    point, attempts = find_valid_curve_point(hash_value, curve)
+    generator = curve.g
+    return point.x == generator.x and point.y == generator.y
 
-    if point:
-        secp192r1_public_key = serialize_point(point)
-        out_path = os.path.join(OUTPUTS_DIR, "aggregation_output.txt")
-        with open(out_path, "w") as out_file:
-            out_file.write(agg_point.format().hex() + '\n')
-            out_file.write(str(attempts) + '\n')
-            out_file.write(secp192r1_public_key.hex())
-        print("Public key aggregation completed successfully and saved to", out_path)
+
+def main():
+    pubkeybtc, pubkeyweak = load_public_keys(PROOF_DIR)
+
+    agg_btc_point = aggregate_btc_pubkeys(pubkeybtc)
+    print("Aggregated btc public key:", agg_btc_point.format().hex())
+
+    agg_weak_point = aggregate_secp192r1_pubkeys(pubkeyweak)
+
+    serial = serialize_point(agg_weak_point)
+    print("Aggregated secp192r1 public key:", serial.hex())
+
+    if is_point_at_infinity_btc(agg_btc_point):
+        print("Error!!! Aggregated public btc key is point at infinity (private key = 0).")
+    elif is_generator_point_btc(agg_btc_point):
+        print("Error!!! Aggregated public btc key is generator (private key = 1).")
     else:
-        print("No valid curve point found.")
+        print("Aggregated public btc key is valid.")
 
-    return secp192r1_public_key.hex()
-
-
+    if is_point_at_infinity_secp192r1(agg_weak_point):
+        print("Error!!! Aggregated public secp192r1 key is point at infinity (private key = 0).")
+        return
+    elif is_generator_point_secp192r1(agg_weak_point):
+        print("Error!!! Aggregated public secp192r1 key is generator (private key = 1).")
+        return
+    else:
+        print("Aggregated public secp192r1 key is valid.")
 
 if __name__ == "__main__":
     main()
